60b578b4e9f616c0997c3c219dae3fc62a1c7d6e
T.Schu <test@t-online.de>
2021-09-11 20:46:58 +0200

    sieb des eratosthenes schleife angelegt

--- src/5_Schleifen/Aufgaben/siebDesEratosthenes.js
+++ src/5_Schleifen/Aufgaben/siebDesEratosthenes.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * Diese Funktion bekommt eine ganze Zahl > 0 herein und gibt alle Primzahlen bis (einschließlich) dieser Zahl zurück
  *
@@ -5,7 +7,22 @@
  * @param n {number}
  * @return {string[]}
  */
-function siebDesEratosthenes(n){
-    //TODO
-    return [n];
+function siebDesEratosthenes(n) {
+    let primtable = Array(n - 1);
+    primtable.fill(true, 0, n - 1);
+    for (let i = 2; i <= n; i++) {
+        console.log(i, primtable[i - 2]);
+        let x = i * 2;
+        while (x <= n) {
+            primtable[x - 2] = false;
+            x = x + i;
+        }
+    }
+    let primnumbers = Array(0);
+    for (let i = 2; i <= n; i++) {
+        if (primtable[i - 2] === true) {
+            primnumbers.push(i);
+        }
+    }
+    return primnumbers;
 }
