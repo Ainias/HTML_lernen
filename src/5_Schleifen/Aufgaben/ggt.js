9b0a78a27ccf04bf6e59133f9a0b6806df29e59c
T.Schu <test@t-online.de>
2021-09-11 19:02:30 +0200

    ggt schleife angelegt

--- src/5_Schleifen/Aufgaben/ggt.js
+++ src/5_Schleifen/Aufgaben/ggt.js
@@ -5,6 +5,12 @@
  * @param y {number}
  * @returns {number}
  */
-function ggt(x,y){
-    return x+y;
+function ggt(x, y) {
+    rest = x % y;
+    while (rest > 0) {
+        x = y;
+        y = rest;
+        rest = x % y;
+    }
+    return y;
 }
